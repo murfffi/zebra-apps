92b3dc81c32e5fbdc07b01ac58af6fea4589ec72
Marin Nozhchev <murfffi@gmail.com>
2021-01-22 22:47:56 +0200

    Angular zebra-demo

---
+++ zebra-demo/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+declare function main(): any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    main();
+  }
+
+  title = 'zebra-demo';
+}
