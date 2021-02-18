c1786150aa7c23ae4e940f987f57ee9d19f6fced
Marin Nozhchev <murfffi@gmail.com>
2021-02-18 14:48:13 +0200

    Ignored a test, that included test code not supported by TeaVM
    Code under test works with TeaVM

--- zebrajs/src/test/java/zebraui/BothTrueTest.java
+++ zebrajs/src/test/java/zebraui/BothTrueTest.java
@@ -1,8 +1,18 @@
 package zebraui;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.teavm.junit.TeaVMTestRunner;
 
 @RunWith(TeaVMTestRunner.class)
 public class BothTrueTest extends zebra4j.fact.BothTrueTest {
+
+	/**
+	 * Ignored because {@link Character.UnicodeBlock} is missing in TeaVM
+	 */
+	@Override
+	@Ignore
+	public void testDescribe() {
+
+	}
 }
