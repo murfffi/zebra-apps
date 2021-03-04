f7bef3e65ffb126ee35bb36f5545e7f2a0febbe1
Marin Nozhchev <murfffi@gmail.com>
2021-03-04 17:49:15 +0200

    Implement foundation for seeded random generation
    Also switch to 0.6-SNAPSHOT of zabra4j

---
+++ zebrajs/src/main/java/zebra4j/apps/SeededRandom.java
@@ -0,0 +1,18 @@
+package zebra4j.apps;
+
+import java.util.Random;
+
+import org.teavm.jso.JSBody;
+
+public class SeededRandom extends Random {
+
+	private static final long serialVersionUID = 1L;
+
+	public SeededRandom(long seed) {
+		seedRandom(String.valueOf(seed));
+	}
+
+	@JSBody(params = { "seed" }, script = "Math.seedrandom(seed);")
+	private static native void seedRandom(String seed);
+
+}
--- zebrajs/src/test/java/zebra4j/apps/ClientTest.java
+++ zebrajs/src/test/java/zebra4j/apps/ClientTest.java
@@ -2,13 +2,15 @@ package zebra4j.apps;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 public class ClientTest {
 
 	@Test
 	public void testGenerateQuestionPuzzleDescription() {
-		PuzzleDescription output = Client.generateQuestionPuzzleDescription(3);
+		PuzzleDescription output = Client.generateQuestionPuzzleDescription(3, new Random(), 1);
 		System.out.println(output);
 		assertNotNull(output);
 	}
