2306917d6841d3bda35649151a27a191c918684f
Marin Nozhchev <murfffi@gmail.com>
2021-03-29 14:50:22 +0300

    Upgrade to zebra4j 0.7-SNAPSHOT

--- zebrajs/src/test/java/zebraui/PuzzleSolverTest.java
+++ zebrajs/src/test/java/zebraui/PuzzleSolverTest.java
@@ -1,9 +1,18 @@
 package zebraui;
 
+import org.chocosolver.solver.ChocoSettings;
 import org.junit.runner.RunWith;
 import org.teavm.junit.TeaVMTestRunner;
 
+import zebra4j.Puzzle;
+import zebra4j.PuzzleSolver;
+
 @RunWith(TeaVMTestRunner.class)
 public class PuzzleSolverTest extends zebra4j.PuzzleSolverTest {
+
+	@Override
+	protected PuzzleSolver createTestSolver(Puzzle puzzle) {
+		return new PuzzleSolver(puzzle, new ChocoSettings());
+	}
 
 }
